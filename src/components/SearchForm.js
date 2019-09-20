9a4f8e2a9d5a9c0c10053606805fda8ec3e6a8d0
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-20 10:05:59 +0100

    import formik and co...

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import { Formik, Form, Field} from 'formik';
 
 export default function SearchForm() {
  
   return (
     <section className="search-form">
      // Add a search form here
+
     </section>
   );
 }
