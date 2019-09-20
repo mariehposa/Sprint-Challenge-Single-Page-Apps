3ca0416c7cc002afdd8b06db5cfbce596f4bfd59
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-20 08:47:38 +0100

    create character card

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({person}) {
+  return 
+    (
+      <span>
+        <h3>Name: {person.name}</h3>
+        <p>Image: {person.image}</p>
+        <p>Species: {person.species}</p>
+        <p>Gender: {person.gender}</p>
+        <p>Status: {person.status}</p>
+      </span>
+    );  
 }
