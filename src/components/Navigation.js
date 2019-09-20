5001ab650295536bd39ce4f30ddb84bd95a288b8
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-20 09:34:03 +0100

    create navigation links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+export default function Navigation () {
+    return (
+        <div>
+            <div>
+                <Link to='/'>Welcome Page</Link>
+            </div>
+            <div>
+                <Link to='/characters'>Character List</Link>
+            </div>
+        </div>
+    );
+}
