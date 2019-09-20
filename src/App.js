94e65dd8d1f5c0c58253c481c0e6fd4e0e1b9f4d
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-20 09:21:52 +0100

    import route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header.js";
 import CharacterList from './components/CharacterList';
+import { Route } from 'react-router-dom';
 
 export default function App() {
   return (
