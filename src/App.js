83cc5406a2fe8ecd8cec974fb8d54d4e0b8659d7
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-20 09:27:19 +0100

    create routes for welcome page and characterlist

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./components/Header.js";
 import CharacterList from './components/CharacterList';
 import { Route } from 'react-router-dom';
+import WelcomePage from './components/WelcomePage'
 
 export default function App() {
   return (
     <main>
       <Header />
       <CharacterList />
+      <Route exact path="/" component={WelcomePage}/>
+      <Route path="/characters" component={CharacterList}/>
     </main>
   );
 }
