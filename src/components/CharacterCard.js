e096f9db50ec95b4f2199f5c827b2d74ff875037
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-20 09:19:45 +0100

    add image

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,13 +1,14 @@
 import React from "react";
+import {CharacterStyle} from './CharacterStyle';
 
 export default function CharacterCard({person}) {
   return (
-      <div>
+      <CharacterStyle>
         <h3>Name: {person.name}</h3>
-        <p>Image: {person.image}</p>
+        <img src= {person.image} />
         <p>Species: {person.species}</p>
         <p>Gender: {person.gender}</p>
         <p>Status: {person.status}</p>
-      </div>
+      </CharacterStyle>
     );  
 }
--- src/components/CharacterStyle.js
+++ src/components/CharacterStyle.js
@@ -1,20 +1,20 @@
 import styled from 'styled-components';
 
-const CharacterStyle = styled.div`
+export const CharacterStyle = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-around;
     margin: 2.5rem;
-    width: 25%;
-    border: .2rem solid ${person.eye_color.split("-")[0]};
-    border-radius: 2rem;
-    color: white;
+    width: 30%;
+    border: .2rem solid ;
+    border-radius: 2rem ;
+    color: green;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 
     &:hover{
         border: .2rem solid white;
         border-radius: 2rem;
-        color: ${person.eye_color.split("-")[0]};
+        color: black;
     }
 
     h3{
@@ -23,7 +23,7 @@ const CharacterStyle = styled.div`
         font-size: 1.5rem;
 
         &:hover{
-            color:${person.eye_color.split("-")[0]};
+            color: green;
             font-size: 1.6rem;
         }
     }
